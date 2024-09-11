69e08103526c9d8d6d4319cb698e380a04f383e0
CommunityBundle <flowerfellmegalovania@gmail.com>
2024-09-11 15:34:03 +0700

    Crazy!!

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -4,20 +4,51 @@ using UnityEngine;
 
 public class Lightning : MonoBehaviour
 {
-    [SerializeField] float minTime = 0.5f;
-    [SerializeField] float threshold = 0.5f;
-    [SerializeField] Light light;
-    private float lastTime = 0;
+    [SerializeField] float minTime = 10.0f;     
+    [SerializeField] float threshold = 0.3f;  
+    [SerializeField] Light lightSource;      
+    [SerializeField] float flashIntensity = 5f; 
+    [SerializeField] float fadeDuration = 1f; 
 
-    private void Update(){
-        if (Time.time - lastTime > minTime){
-            if (Random.value > threshold){
-                light.enabled = true;
-                lastTime = Time.time; 
-            } else {
-                light.enabled = false;
-                lastTime = Time.time; 
+    private float lastTime = 0;
+    private Coroutine fadeCoroutine;
+
+    private void Update()
+    {
+        if (Time.time - lastTime > minTime)
+        {
+            if (Random.value > threshold)
+            {
+                if (fadeCoroutine != null)
+                {
+                    StopCoroutine(fadeCoroutine);  
+                }
+                
+                lightSource.intensity = flashIntensity;
+                lightSource.enabled = true;           
+                lastTime = Time.time;
+
+                fadeCoroutine = StartCoroutine(FadeLightning());
+            }
+            else
+            {
+                lastTime = Time.time;
             }
         }
+    }
+
+    private IEnumerator FadeLightning()
+    {
+        float startIntensity = lightSource.intensity;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            lightSource.intensity = Mathf.Lerp(startIntensity, 0f, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        lightSource.enabled = false;  
     }
 }
