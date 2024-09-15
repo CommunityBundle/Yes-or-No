141edbe910158b9506c81442589a72ba84c326bb
CommunityBundle <flowerfellmegalovania@gmail.com>
2024-09-15 11:27:51 +0700

    Latest

---
+++ Assets/EndState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndState : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource rainSource;
+    [SerializeField] AudioSource thunderSource;
+    [SerializeField] AudioSource JumpScare;
+
+    void Start(){
+        rainSource.enabled = false;
+        thunderSource.enabled = false;
+        JumpScare.PlayDelayed(0.01f);
+        StartCoroutine(JumpScareSpawn());
+    }
+    IEnumerator JumpScareSpawn(){
+        yield return new WaitForSeconds(3f);
+        audioSource.PlayDelayed(0.1f);
+        StartCoroutine(EndGame());
+    }
+    IEnumerator EndGame(){
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene("MainMenu");
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+}
